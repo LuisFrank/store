770f8c636bef08ebcc5a47bff9fb90f22a805cf1
luis frank florentino medrano <u201519781@upc.edu.pe>
2018-11-10 18:07:53 -0500

    change assets false

--- config/application.rb
+++ config/application.rb
@@ -11,12 +11,14 @@ module Store
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
+    config.assets.initialize_on_precompile = false
+    config.assets.enabled = true
+    
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
-    config.assets.initialize_on_precompile = false
-    config.assets.enabled = true
+
   end
 end
