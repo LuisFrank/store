d8cbd6f1035c92506d077f68f1dbe93c1ee90dff
luis frank florentino medrano <u201519781@upc.edu.pe>
2018-11-10 18:53:48 -0500

    exclude fonts

--- config/application.rb
+++ config/application.rb
@@ -14,7 +14,7 @@ module Store
 
     config.assets.initialize_on_precompile = true
 
-    config.assets.paths << Rails.root.join("app", "assets", "fonts")
+    config.assets.paths << Rails.root.join("app", "assets")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
