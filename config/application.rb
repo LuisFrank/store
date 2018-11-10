da223804be1d5729735915a4ef770d1e6e41103b
luis frank florentino medrano <u201519781@upc.edu.pe>
2018-11-10 18:29:13 -0500

    commet initializers

--- config/application.rb
+++ config/application.rb
@@ -11,9 +11,9 @@ module Store
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
 
-    config.assets.initialize_on_precompile = false
-    config.assets.enabled = true
-    
+
+    config.assets.initialize_on_precompile = true
+
     config.assets.paths << Rails.root.join("app", "assets", "fonts")
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
